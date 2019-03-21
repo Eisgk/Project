a99da53929811d0e476b6a067e22bfe245898e8d
Eisgk <game.080.pf@gmail.com>
2019-03-21 22:02:08 +0700

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void run(){
+    for(int i = 0; i < 16; i++)
+    {
+        for(int j = 0; j < 16; j++)
+        {
+           seat[i][j] = " ";
+        }
+        
+    }
+    while(m){
+        cout << "---------------------------- C I N E M A-----------------------------\n\n";
+        cout << "Menu : ";
+        cout << "1.Choose movie and seat";
+        cout << "2.Check movie and seat";
+        cout << "3.Cancel seat";
+        cout << "4.Exit";
+        cout << "Menu you choose : ";
+        int menu_index;
+        cin >> menu_index ;
+        switch (menu_index)
+        {
+            case 1
+                break;
+        
+            default:
+                break;
+        }
+    }
+    
+    
+}
