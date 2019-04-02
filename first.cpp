496e105e34b348fde94fd9d347d132767d44b641
lab303-23 <lab303-23@labeng.local>
2019-04-02 16:58:56 +0700

    1

--- first.cpp
+++ first.cpp
@@ -1,36 +1,3 @@
-#include <iostream>
-using namespace std;
-
-void run(){
-    for(int i = 0; i < 16; i++)
-    {
-        for(int j = 0; j < 16; j++)
-        {
-           //seat[i][j] = " ";
-        }
-        
-    }
-    while(1){
-        cout << "---------------------------- C I N E M A-----------------------------\n\n";
-        cout << "Menu : ";
-        cout << "1.Choose movie and seat";
-        cout << "2.Check movie and seat";
-        cout << "3.Cancel seat";
-        cout << "4.Exit";
-        cout << "Menu you choose : ";
-        int menu_index;
-        cin >> menu_index ;
-        /*switch (menu_index)
-        {
-            case 1
-                //break;
-        
-            default:
-                break;
-        }*/
-    }
-    
-    
-}
+/*#include <iostream>
 
 /////////////////// test/ ////////////////////////// test push
